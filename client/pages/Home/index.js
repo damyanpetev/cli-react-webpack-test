c594881b9c3b7b2c8f47773072b7255330e6efff
Damyan Petev <damyan.petev@gmail.com>
2018-03-07 06:12:12 +0200

    Initial commit for project: react-app2

---
+++ client/pages/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from 'components/Header';
+
+export default () => (
+  <article className="home">
+    <Header title="Home" className="header-dark" />
+    <h2>Ignite UI CLI Default empty project structure for React: react-app2</h2>
+    <img src="https://static.infragistics.com/marketing/Website/products/ignite/Ignite-header-apps-960.png" alt="Ignite UI" height="402" width="402" />
+  </article>
+);
